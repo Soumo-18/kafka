f5e90d1844244a9904e28adb40db93ece1915ff4
Soumo-18 <soumomandal7@gmail.com>
2026-05-03 02:00:19 +0530

    Refactor Kafka client configuration to use environment variables for brokers and authentication

--- kafka-client.js
+++ kafka-client.js
@@ -1,6 +1,22 @@
 import { Kafka } from "kafkajs";
 
-export const kafkaClient = new Kafka( {
-    clientId:'chaicode',
-    brokers:['localhost:9092']
-})
+// export const kafkaClient = new Kafka( {
+//     clientId:'chaicode',
+//     brokers:['localhost:9092']
+// })
+const brokers = [process.env.KAFKA_BROKER || 'localhost:9092'];
+
+const saslConfig = process.env.KAFKA_USERNAME ? {
+    ssl: true,
+    sasl: {
+        mechanism: 'scram-sha-256', 
+        username: process.env.KAFKA_USERNAME,
+        password: process.env.KAFKA_PASSWORD,
+    }
+} : {}; 
+
+export const kafkaClient = new Kafka({
+    clientId: 'live-location-tracker',
+    brokers: brokers,
+    ...saslConfig
+});
